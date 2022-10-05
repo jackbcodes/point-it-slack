dce2cc5f10d257689d672b62cb2b1e1076938013
Jack Brown <brown_jack80@hotmail.com>
2022-10-05 18:18:11 +0100

    Adding TS and Prettier

---
+++ functions/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 120,
+  arrowParens: 'always',
+}
---
+++ functions/src/index.ts
@@ -0,0 +1,29 @@
+import { config, https } from 'firebase-functions'
+import { App, ExpressReceiver } from '@slack/bolt'
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: config().slack.signing_secret,
+  endpoints: '/events',
+  processBeforeResponse: true,
+})
+const app = new App({
+  receiver: expressReceiver,
+  token: config().slack.bot_token,
+  processBeforeResponse: true,
+})
+// Global error handler
+app.error(console.log)
+
+// Handle `/echo` command invocations
+app.command('/echo-from-firebase', async ({ command, ack, say }) => {
+  // Acknowledge command request
+  await ack()
+
+  // Requires:
+  // Add chat:write scope + invite the bot user to the channel you run this command
+  // Add chat:write.public + run this command in a public channel
+  await say(`You said "${command.text}"`)
+})
+
+// https://{your domain}.cloudfunctions.net/slack/events
+export const slack = https.onRequest(expressReceiver.app)
